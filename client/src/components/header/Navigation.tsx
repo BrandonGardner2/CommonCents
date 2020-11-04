780ed802a678b1054c1ed0c81c88844c11e8a4db
Brandon Gardner <brandon.gardner2@outlook.com>
2020-11-03 19:11:28 -0600

    fixes dashboard navigation. confirms auth0 login works. changes login/logout buttons to just match other menu items

--- client/src/components/header/Navigation.tsx
+++ client/src/components/header/Navigation.tsx
@@ -2,31 +2,34 @@ import React, { FC } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { Link, useLocation } from 'react-router-dom';
 import { Menu } from 'antd';
-import { HomeOutlined, PieChartOutlined } from '@ant-design/icons';
-import LoginBtn from '../auth/LoginBtn';
-import LogoutBtn from '../auth/LogoutBtn';
+import { HomeOutlined, PieChartOutlined, LoginOutlined, LogoutOutlined } from '@ant-design/icons';
 import './headerStyle.scss';
 
 const Navigation: FC = function () {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
   const location = useLocation();
 
+  const handleLogin = () => loginWithRedirect();
+  const handleLogout = () => logout({ returnTo: window.location.origin });
+
+  const { pathname } = location;
   return (
-    <Menu
-      className='navigation-container'
-      mode='horizontal'
-      selectedKeys={[location.pathname]}
-      theme='dark'
-    >
+    <Menu className='navigation-container' mode='horizontal' selectedKeys={[pathname]} theme='dark'>
       <Menu.Item key='/' icon={<HomeOutlined />}>
         <Link to='/'>Home</Link>
       </Menu.Item>
       {isAuthenticated && (
         <Menu.Item key='/dashboard' icon={<PieChartOutlined />}>
-          <Link to='/dashbaord'>Dashboard</Link>
+          <Link to='/dashboard'>Dashboard</Link>
         </Menu.Item>
       )}
-      <Menu.Item key='login-out'>{isAuthenticated ? <LogoutBtn /> : <LoginBtn />}</Menu.Item>
+      <Menu.Item
+        key='login-out'
+        icon={isAuthenticated ? <LogoutOutlined /> : <LoginOutlined />}
+        onClick={isAuthenticated ? handleLogout : handleLogin}
+      >
+        {isAuthenticated ? 'Logout' : 'Login'}
+      </Menu.Item>
     </Menu>
   );
 };
